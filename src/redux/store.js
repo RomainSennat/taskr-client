3605c25ae4aeaa40dfbc9302694d4d0fdade35d1
Romain Sennat <romainsennat@users.noreply.github.com>
2019-08-12 15:22:21 +0200

    Add redux-dev-tools.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function buildStore() {
-    const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    const store = createStore(reducers, composeWithDevTools({ trace: true, traceLimit: 25 })());
     if (process.env.NODE_ENV !== 'production') {
         if (module.hot) {
             module.hot.accept('./reducers', () => {
